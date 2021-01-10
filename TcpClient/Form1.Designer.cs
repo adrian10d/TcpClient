c7a754e0cc3c129e9cfe02b7b5d9541e8fad802c
osklix <lukas@DESKTOP-LSH08S0>
2021-01-11 00:01:58 +0100

    Drobne poprawki

--- TcpClient/Form1.Designer.cs
+++ TcpClient/Form1.Designer.cs
@@ -32,15 +32,17 @@ namespace Client
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Impact", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(60, 44);
+            this.label1.Location = new System.Drawing.Point(45, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(231, 41);
+            this.label1.Size = new System.Drawing.Size(187, 34);
             this.label1.TabIndex = 0;
             this.label1.Text = "Masz już konto?";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -48,9 +50,10 @@ namespace Client
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(27, 198);
+            this.button1.Location = new System.Drawing.Point(20, 161);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(362, 117);
+            this.button1.Size = new System.Drawing.Size(272, 95);
             this.button1.TabIndex = 1;
             this.button1.Text = "Tak, chcę się zalogować";
             this.button1.UseVisualStyleBackColor = true;
@@ -59,23 +62,37 @@ namespace Client
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button2.Location = new System.Drawing.Point(413, 198);
+            this.button2.Location = new System.Drawing.Point(310, 161);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(362, 117);
+            this.button2.Size = new System.Drawing.Size(272, 95);
             this.button2.TabIndex = 2;
             this.button2.Text = "Nie, chcę się zarejestrować";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(198, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(207, 64);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Zakończ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(856, 479);
+            this.ClientSize = new System.Drawing.Size(642, 389);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Check Month";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -89,6 +106,7 @@ namespace Client
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- TcpClient/Form1.cs
+++ TcpClient/Form1.cs
@@ -41,5 +41,10 @@ namespace Client
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
